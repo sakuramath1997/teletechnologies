72c60438ab6ee2b27296ec7df6ccd7117c9ec315
sakuramath1997 <takahashi.1997.jp@gmail.com>
2024-11-12 11:05:12 +0900

    remove some library which is not used

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
         <link rel="stylesheet" type="text/css" href="/assets/css/reset.css">
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
         <title>teletechnologies</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <script src="/assets/js/cls/EventListener.js"></script>
         <script src="/assets/js/cls/ScrollManager.js"></script>
         <script src="/assets/js/cls/ClassToggler.js"></script>
